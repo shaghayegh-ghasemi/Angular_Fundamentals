f114ed2e4e3a04b51c91faa303f8b5912fff05ea
Shaghayegh Ghasemi <shaghayegh.ghasemi@bounteous.com>
2025-04-24 14:58:15 -0400

    conditional display added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { ProfileCardComponent } from "./profile-card/profile-card.component";
 import { LikeButtonComponent } from './like-button/like-button.component';
 import { InputDisplayComponent } from './input-display/input-display.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent, LikeButtonComponent, InputDisplayComponent],
+  imports: [ProfileCardComponent, LikeButtonComponent, InputDisplayComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-practice';
+
+  showDetails: boolean = false;
+
+  toggleDetails(): void {
+    this.showDetails = !this.showDetails;
+  }
 }
