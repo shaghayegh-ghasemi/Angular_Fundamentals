63afc7af59c385601b999cfeb78d186ac486752f
Shaghayegh Ghasemi <shaghayegh.ghasemi@bounteous.com>
2025-04-27 14:29:11 -0400

    simple calculator added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router'; 
 
 import { ProductListComponent } from './products/product-list/product-list.component'; 
+import { CalculatorComponent } from './calculator/calculator.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([
-      { path: 'products', component: ProductListComponent }
+      { path: 'products', component: ProductListComponent },
+      { path: 'calculator', component: CalculatorComponent }
     ])  
   ]
 };
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [FormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  a: number = 0;
+  b: number = 0;
+  result: number = 0;
+
+  add(){
+    this.result = this.a + this.b;
+  }
+
+  subtract(){
+    this.result = this.a - this.b;
+  }
+
+  multiply(){
+    this.result = this.a * this.b;
+  }
+
+  divide(){
+    if (this.b !== 0){
+      this.result = this.a / this.b;
+    } else {
+      alert("Division by zero!");
+    }
+  }
+}
